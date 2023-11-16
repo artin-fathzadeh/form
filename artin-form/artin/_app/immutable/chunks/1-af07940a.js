3b61d81031a684a6044d418372234abb191445f4
artin <99147750+artin-fathzadeh@users.noreply.github.com>
2023-11-15 20:58:41 -0800

    gen

---
+++ artin-form/artin/_app/immutable/chunks/1-af07940a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_error.svelte-a7374783.js";export{t as component};
---
+++ artin-form/artin/_app/immutable/chunks/4-3464948b.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/f/_slug_/_page.svelte-4ea1998d.js";const e=!0;export{t as component,e as server};
